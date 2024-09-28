0bef02c75f2e1611c50ea7973968921cf0f39d91
elliott <elliott>
2024-09-28 16:40:39 +0300

    Initial commit

---
+++ CoreDataWithMVVM/CoreDataWithMVVM/CoreDataWithMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataWithMVVMApp.swift
+//  CoreDataWithMVVM
+//
+//  Created by Мадина Валиева on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataWithMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

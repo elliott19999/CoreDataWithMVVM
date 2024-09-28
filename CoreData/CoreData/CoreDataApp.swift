0231dd50f3d2a83fc8992f1d8474e6a4e1add64c
elliott <elliott>
2024-09-28 16:44:53 +0300

    no message

---
+++ CoreData/CoreData/CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataApp.swift
+//  CoreData
+//
+//  Created by Мадина Валиева on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

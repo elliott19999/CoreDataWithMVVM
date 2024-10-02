7fdf0e8f934062b1e22907bbd0d60d94c6ee9d70
elliott <elliott>
2024-10-02 20:53:22 +0300

    no message

--- CoreDataW/CoreDataW/CoreDataBootCamp.swift
+++ CoreDataW/CoreDataW/CoreDataBootCamp.swift
@@ -9,11 +9,17 @@ import SwiftUI
 import CoreData
 class CoreDataViewModel: ObservableObject {
     
+    
+//    Это объект, который управляет моделью данных, хранением и контекстом для взаимодействия с базой данных.
     let container: NSPersistentContainer
+    
     @Published var savedEntities: [FruitEntity] = []
     
+//    При инициализации контейнера задается имя модели данных ("FruitsContainer"), которая должна соответствовать имени, заданному в файле .xcdatamodeld.
     init() {
         container = NSPersistentContainer(name: "FruitsContainer")
+        
+//        вызывается метод loadPersistentStores, который загружает хранилище данных (базу данных). Если возникает ошибка во время загрузки, она выводится в консоль, иначе выводится сообщение об успешной загрузке.
         container.loadPersistentStores { (description, error) in
             if let error = error {
                 print("error loading \(error)")
@@ -23,14 +29,22 @@ class CoreDataViewModel: ObservableObject {
         }
         fetchFruits()
     }
+//    Этот метод создаёт запрос к базе данных с целью извлечь все объекты сущности FruitEntity.
     func fetchFruits() {
+        
+//        NSFetchRequest<FruitEntity>(entityName: "FruitEntity") – запрос, который возвращает все записи из сущности FruitEntity (то есть таблицы с фруктами).
         let request = NSFetchRequest<FruitEntity>(entityName: "FruitEntity")
+//        Если запрос выполнен успешно, массив объектов savedEntities обновляется с новыми данными. В случае ошибки выводится сообщение в консоль.
         do {
-          savedEntities =  try  container.viewContext.fetch(request)
+          savedEntities =  try container.viewContext.fetch(request)
         } catch let error {
             print("error fetching \(error)")
         }
     }
+    
+//    Этот метод создаёт новый объект сущности FruitEntity в контексте (context) Core Data.
+//    После создания объекта ему присваивается значение, переданное в параметре text (в поле name).
+//    Далее вызывается метод saveData(), который сохраняет изменения в базе данных.
     func addFruit(text: String) {
         let newFruit = FruitEntity(context: container.viewContext)
         newFruit.name = text
@@ -96,3 +110,6 @@ struct CoreDataBootCamp: View {
 #Preview {
     CoreDataBootCamp()
 }
+
+
+//Чтобы актуализировать список фруктов, вызывается метод fetchFruits(), который заново загружает все записи из базы данных. Это позволяет интерфейсу обновиться и отобразить самые последние данные, включая все изменения, сделанные в процессе добавления или удаления фруктов.
